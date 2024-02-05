106fa5e9cbd9d812ae3dfd9a798bddf3e36253c5
francksefu <francksefu1998@gmail.com>
2024-02-06 00:59:41 +0200

    Add new Recipe form, And set up the index file

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,12 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    user ||= User.new
+
+    can :destroy, Recipe, user_id: user.id
+
+    can :destroy, Food, user_id: user.id
+    can :update, Recipe, user_id: user.id
     # Define abilities for the user here. For example:
     #
     #   return unless user.present?
