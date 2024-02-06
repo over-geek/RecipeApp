4492b588228ac0d54055bdc9dcf40d837415fbd8
over-geek <kenagy100@gmail.com>
2024-02-06 01:03:36 +0000

    Modify model associations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,23 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+  has_many :recipe_foods, dependent: :destroy
+  has_many :foods, through: :recipe_foods, dependent: :destroy
+
+  def calculate_total_amount
+    total_amount = 0
+    recipe_foods.each do |recipe_food|
+      total_amount += recipe_food.food.price * recipe_food.quantity
+    end
+    total_amount
+  end
+
+  # scope :public_recipes, -> { where(public: true) }
+
+  # def public?
+  #   is_public
+  # end
+
+  validates :name, presence: true, length: { in: 0..250 }
+  validates :preparation_time, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :cooking_time, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
